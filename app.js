d27b1240ff7b5d6aefada81117a2f092b42e5888
sangyunpark99 <psangyun5108@gmail.com>
2023-04-24 13:50:30 +0900

    pagenation

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENTS_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  // 공유되는 자원
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -13,15 +17,18 @@ function getData(url) {
 
 function newsFeed() {
   // 글 목록 화면
-  const newsFeed = getData(NEWS_URL); // newsFeed 데이터 받아오기ß
+  const newsFeed = getData(NEWS_URL); // newsFeed 데이터 받아오기
+
+  console.log(newsFeed);
   const newsList = [];
 
   newsList.push("<ul>");
 
-  for (let i = 0; i < newsFeed.length; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    // 한페이지에 10개씩 보여주기
     newsList.push(`
     <li>
-      <a href="#${newsFeed[i].id}">
+      <a href="#/show/${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
@@ -29,33 +36,46 @@ function newsFeed() {
   }
 
   newsList.push("</ul>");
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }">이전 페이지</a>
+      <a href="#/page/${
+        store.currentPage < 3 ? store.currentPage + 1 : 3
+      }">다음 페이지</a>
+    </div>
+  `);
   container.innerHTML = `${newsList.join("")}`;
 }
 
 function newsDetail() {
-  const id = location.hash.substring(1); // 1번째 index부터 사용
+  const id = location.hash.substring(7); // 1번째 index부터 사용
 
   const newsContent = getData(CONTENTS_URL.replace("@id", id));
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
 
 function router() {
   const routePath = location.hash;
-  console.log(routePath);
 
   if (routePath === "") {
     // location.hash에 #만 들어있는 경우 빈문자열을 반환한다.
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    // #/page/라는 값이 존재한다면
+    store.currentPage = Number(routePath.substring(7)); // 7번째 index부터 사용
     newsFeed();
   } else {
     newsDetail();
   }
 }
 
-window.addEventListener("hashchange", router);
+window.addEventListener("hashchange", router); // hash값이 변할때마다 event 호출
 router();
--- dist/index.9f6fdb5c.js
+++ dist/index.9f6fdb5c.js
@@ -562,6 +562,10 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENTS_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+    // 공유되는 자원
+    currentPage: 1
+};
 function getData(url) {
     ajax.open("GET", url, false);
     ajax.send();
@@ -569,37 +573,48 @@ function getData(url) {
 } // 요청 보내기
 function newsFeed() {
     // 글 목록 화면
-    const newsFeed = getData(NEWS_URL); // newsFeed 데이터 받아오기ß
+    const newsFeed = getData(NEWS_URL); // newsFeed 데이터 받아오기
+    console.log(newsFeed);
     const newsList = [];
     newsList.push("<ul>");
-    for(let i = 0; i < newsFeed.length; i++)newsList.push(`
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++)// 한페이지에 10개씩 보여주기
+    newsList.push(`
     <li>
-      <a href="#${newsFeed[i].id}">
+      <a href="#/show/${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
   `);
     newsList.push("</ul>");
+    newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href="#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
+    </div>
+  `);
     container.innerHTML = `${newsList.join("")}`;
 }
 function newsDetail() {
-    const id = location.hash.substring(1); // 1번째 index부터 사용
+    const id = location.hash.substring(7); // 1번째 index부터 사용
     const newsContent = getData(CONTENTS_URL.replace("@id", id));
     container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
 function router() {
     const routePath = location.hash;
-    console.log(routePath);
     if (routePath === "") // location.hash에 #만 들어있는 경우 빈문자열을 반환한다.
     newsFeed();
-    else newsDetail();
+    else if (routePath.indexOf("#/page/") >= 0) {
+        // #/page/라는 값이 존재한다면
+        store.currentPage = Number(routePath.substring(7)); // 7번째 index부터 사용
+        newsFeed();
+    } else newsDetail();
 }
-window.addEventListener("hashchange", router);
+window.addEventListener("hashchange", router); // hash값이 변할때마다 event 호출
 router();
 
 },{}]},["62IOj","2eZjZ"], "2eZjZ", "parcelRequire94c2")
